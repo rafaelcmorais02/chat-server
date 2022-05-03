2bfd5d34f7fc0b0c69b78ceb0c09a9f1591cb8b2
rafaelmoraiscunha <rafael.morais@franq.com.br>
2022-05-02 21:46:04 -0300

    Feat: habilita cors para todas as origens

--- config/settings.py
+++ config/settings.py
@@ -45,17 +45,21 @@ INSTALLED_APPS = [
     'message',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'config.urls'
 
