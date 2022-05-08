d55114210c9317c392b951e05a3723a6589761aa
rafaelmoraiscunha <rafael.morais@franq.com.br>
2022-05-08 14:01:52 -0300

    Fix: adiciona caminho relativo no arquivo docker-compose.yml

--- reset-db.sh
+++ reset-db.sh
@@ -2,13 +2,13 @@
 # Resets the local Django database, adding an admin login and migrations
 set -e
 echo -e "\n>>> Resetting the database"
-sudo /home/ubuntu/chat-server/docker-compose -f docker-compose.prod.yml run chat-server ./manage.py reset_db --close-sessions --noinput
+sudo docker-compose -f /home/ubuntu/chat-server/docker-compose.prod.yml run chat-server ./manage.py reset_db --close-sessions --noinput
 # Stop docker containers
 echo -e "\nStopping all running Docker containers"
-sudo /home/ubuntu/chat-server/docker-compose -f docker-compose.prod.yml down
+sudo docker-compose -f /home/ubuntu/chat-server/docker-compose.prod.yml down
 # Start docker containers
 echo -e "\nStarting Docker containers"
-sudo /home/ubuntu/chat-server/docker-compose -f docker-compose.prod.yml up -d
+sudo docker-compose -f /home/ubuntu/chat-server/docker-compose.prod.yml up -d
 # Making migrations
 echo -e "\n>>> Running migrations"
-sudo /home/ubuntu/chat-server/docker-compose -f docker-compose.prod.yml run chat-server ./manage.py migrate
+sudo docker-compose -f /home/ubuntu/chat-server/docker-compose.prod.yml run chat-server ./manage.py migrate
