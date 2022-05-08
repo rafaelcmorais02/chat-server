0eab847bc18d839859dc086ee665c74b9cbbef70
rafaelmoraiscunha <rafael.morais@franq.com.br>
2022-05-08 08:43:07 -0300

    Feat: altera dominio para nome de dominio definitivo

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(chat.emillieerafael.com)
+domains=(chatapi.portfolio-rafael.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
